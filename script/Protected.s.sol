056a4e6ad3335108620feae2923b1aabb1241ecf
Andreas Bigger <abigger87@gmail.com>
2022-07-10 15:34:45 -0700

    test

--- script/Protected.s.sol
+++ script/Protected.s.sol
@@ -7,16 +7,22 @@ import "forge-std/console2.sol";
 import {Surl} from "surl/Surl.sol";
 import {Protec} from "src/Protec.sol";
 
-contract ProtectedScript is Script {
-    using Protec for *;
+contract ProtectedScript is Script, Protec {
     using Surl for *;
 
     function setUp() public {}
 
-    function run() public {
+    function func() internal {
         // Perform a simple get request
         console2.log("Getting request...");
         (uint256 status, bytes memory data) = "https://httpbin.org/get".get();
         console2.log(string.concat("Status: ", string(abi.encode(status))));
+        console2.log(string.concat("Bytes Result: ", string(data)));
     }
+
+    function run() public {
+        function () internal g = func;
+        safe(g);
+    }
+
 }
--- src/Protec.sol
+++ src/Protec.sol
@@ -2,13 +2,43 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/console2.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {strings} from "strings/strings.sol";
 
-library Protec {
-  function ffi(string memory input) public returns (bytes memory ffi_res) {
-    console2.log("[BLOCKED FFI CALL]");
-    bytes memory encoded = abi.encode(0, "BLOCKED_FFI_CALL");
-    (uint256 success, bytes memory res) = abi.decode(encoded, (uint256, bytes));
-    console2.log(string.concat("Is success: ", string(abi.encode(success))));
-    return encoded;
+contract Protec {
+  using strings for *;
+
+  modifier unsafe() {
+    Vm tvm = Vm(address(bytes20(uint160(uint256(keccak256("hevm cheat code"))))));
+
+    bool init_ffi = false;
+    try tvm.envBool("FOUNDRY_FFI") returns (bool ffi) {
+      ffi = init_ffi;
+    } catch (bytes memory reason) {
+      // Try to read "ffi" from the foundry toml
+      string memory toml = tvm.readFile("foundry.toml");
+      strings.slice memory s = toml.toSlice();
+      if (s.contains("ffi=".toSlice())) {
+        s.split("ffi=".toSlice());
+        init_ffi = s.split("\n".toSlice()).equals("true".toSlice());
+      } else if (s.contains("ffi =".toSlice())) {
+        s.split("ffi =".toSlice());
+        init_ffi = s.split("\n".toSlice()).equals("true".toSlice());
+      }
+    }
+
+    // Set the ffi env var
+    tvm.setEnv("FOUNDRY_FFI", "false");
+    _;
+    if (init_ffi) {
+      tvm.setEnv("FOUNDRY_FFI", "true");
+    } else {
+      tvm.setEnv("FOUNDRY_FFI", "false");
+    }
+
+  }
+
+  function safe(function() internal func) internal unsafe {
+    func();
   }
 }
